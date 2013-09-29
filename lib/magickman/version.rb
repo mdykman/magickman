31f6a9767aae679908441c80a3d270c443490823
Michael Dykman <mdykman@gmail.com>
2013-09-29 19:01:23 -0400

    some cleanup

--- lib/magickman/version.rb
+++ lib/magickman/version.rb
@@ -1,3 +1,3 @@
 module MagickMan
-  VERSION = "0.0.62"
+  VERSION = "0.0.64"
 end
--- lib/rails/app/controllers/magick_man/magick_man_controller.rb
+++ lib/rails/app/controllers/magick_man/magick_man_controller.rb
@@ -7,25 +7,21 @@ module MagickMan
 
     def serve
       # remove the first component
-      #    filepath= :imgpath.to_s
       filepath= "#{params[:imgpath]}.#{params[:format]}"
-      puts "loading from req path #{filepath}"
+#      puts "loading from req path #{filepath}"
 
       mm = MagickManager.instance
 
       if not mm.supportsType? params[:format]
-        puts "unsupported type requested: #{params[:format]}"
+#        puts "unsupported type requested: #{params[:format]}"
         redirect_to filepath, status: 302
         return
       end
 
       img = mm.findimage(filepath)
       if not img
-        puts "#{__FILE__} #{__LINE__}"
         src = mm.findsource filepath
-        puts "#{__FILE__} #{__LINE__}"
         if src
-          puts "#{__FILE__} #{__LINE__}"
           rr = mm.convert src
           if rr
             img = src[:target]
@@ -34,7 +30,7 @@ module MagickMan
       end
 
       if File.exists? img
-        puts "serving #{img}"
+#        puts "serving #{img}"
         expires_in 30.days, :public => true
         send_file img, :disposition => params[:attachment] ?  'attachment' : 'inline'
         return
@@ -43,10 +39,7 @@ module MagickMan
         puts "redirecting to #{filepath} #{__LINE__}"
         redirect_to img, status: 303
         return
-        puts "#{__FILE__} #{__LINE__}"
-        #       raise ActionController::RoutingError.new('Not Found')
       end
-      puts "#{__FILE__} #{__LINE__}"
     end  # end of serve
   end
 
